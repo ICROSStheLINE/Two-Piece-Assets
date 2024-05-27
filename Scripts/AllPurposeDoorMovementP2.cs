731cf43c92ee608fe3630f6b399cdd25e6d3f2b5
CoolWillRule <coolwillrule123@gmail.com>
2024-05-26 22:00:05 -0400

    Louise Basic Dialogue Code (No Saving)
    Basic ass dialogue lul

--- Scripts/AllPurposeDoorMovementP2.cs
+++ Scripts/AllPurposeDoorMovementP2.cs
@@ -29,7 +29,7 @@ public class AllPurposeDoorMovementP2 : MonoBehaviour
 	
 	
 	void OnTriggerEnter2D(Collider2D collision)
-	{
+	{	
 		if (collision.gameObject.tag == "Player")
 		{
 			if (allPurposeDoorMovement.gateMustBeClosed == false)
